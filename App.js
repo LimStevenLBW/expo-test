6d9ee32416c1d2037cdd980ac82994ec6ee5a10f
Steven Lim <limstevenlbw@gmail.com>
2019-01-23 03:03:57 -0800

    content commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Ravioli Ravioli, please give me the end i deservioli</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
